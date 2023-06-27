c446ee33b6b9cee12cc2e74b12525c8e425960b2
Vimal Vs <vimalchan24@gmail.com>
2023-06-27 21:03:37 +0530

    project updates

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -2,8 +2,10 @@ import projects from '../projectsData.json'
 import Image from 'next/image'
 import { BsArrowRightShort } from "react-icons/bs";
 import { FiArrowUpRight } from "react-icons/fi";
+import { AiOutlineGithub } from "react-icons/ai";
 
 export default function Projects(){
+
     return (
         <section id="projects" className="lg:mt-2 lg:scroll-mt-24">
             <div id="projects-section">
@@ -11,15 +13,27 @@ export default function Projects(){
                     projects.map(project => {
                         return(
                             <a id='project' key={ project.id } href={ project.link } className="group flex flex-col gap-6 mt-6 p-2 lg:flex-row lg:gap-12 lg:p-4 hover:text-teal-300 focus-visible:text-teal-300 hover:bg-slate-500 hover:bg-opacity-10 transition-all rounded-md">
-                                <div>
-                                    <Image src={ project.picture } loading="lazy" width="300" height="100" alt="project-demo-image" className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30'/>
+                                <div className='mt-2'>
+                                    <Image src={ project.picture } width="300" height="100" alt="project-demo-image" className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30'/>
                                 </div>
-                                <div className='w-256'>
-                                    <div className='flex gap-1.5'>
-                                        <h3 className="text-lg">{ project.title } </h3>
-                                        <span className='mt-2 group-hover:translate-x-1 group-hover:-translate-y-1 transition-all'><FiArrowUpRight /></span>
+                                <div>
+                                    <div className='flex justify-between'>
+                                        <div className='flex gap-1.5'>
+                                            <h3 className="text-lg">{ project.title } </h3>
+                                            <span className='mt-2 group-hover:translate-x-1 group-hover:-translate-y-1 transition-all'><FiArrowUpRight /></span>
+                                        </div>
+                                        <button onClick={() => gitRedirect() } className='mt-1'><AiOutlineGithub /></button>
                                     </div>
                                     <p className="text-slate-400 mt-3 lg:leading-normal">{ project.description }</p>
+                                    <ul className="flex flex-wrap">
+                                        { project.technology.map(tech => {
+                                        return (
+                                            <li className="mt-1.5 mr-2" key={ tech }>
+                                                <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-sm font-medium leading-5 text-teal-300">{ tech }</div>
+                                            </li>
+                                            )
+                                        })}
+                                    </ul>
                                 </div>
                             </a>
                         )
