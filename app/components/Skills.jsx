6a1a29be5dfd1c338fa70f238cba4731831dc78d
Vimal Vs <vimalchan24@gmail.com>
2023-11-29 18:54:29 +0530

    UI changes

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -2,13 +2,13 @@ import Contact from "./Contact";
 
 export default function Skills() {
     return (
-        <section id="skills" className="mt-6 lg:mt-24 lg:scroll-mt-24 text-lg text-slate-400">
+        <section id="skills" className="lg:pb-12 lg:scroll-mt-24 text-lg text-slate-400">
             <div className="flex flex-col gap-1 mt-4">
                 <h4 className="text-slate-300">Programming Languages :</h4>
                 <div className="flex mt-4 ml-4">
                     <ul className="flex flex-col gap-4 list-disc w-1/3">
-                        <li>C++</li>
                         <li>JavaScript</li>
+                        <li>C++</li>
                     </ul>
                     <ul className="flex flex-col ml-20 gap-4 list-disc">
                         <li>Python</li>
@@ -28,7 +28,7 @@ export default function Skills() {
                         <li>Next.Js</li>
                         <li>Prisma</li>
                         <li>MongoDB</li>
-                        <li>Postgres</li>
+                        <li>MySQL</li>
                     </ul>
                 </div>
             </div>
