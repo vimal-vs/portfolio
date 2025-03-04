91118ec469ec4921eabb44e302d674541ccd4794
vimal-vs <vimalchan24@gmail.com>
2025-03-04 20:33:40 +0530

    site-map config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://www.vimalvs.com",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "weekly",
   priority: 0.7,
+  generateIndexSitemap: true,
 };
